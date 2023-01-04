f9f643fd9f90373614371c29e7ea1eb63db59c10
olly222 <adelabijacob@gmail.com>
2023-01-04 03:46:22 -0800

    More Classes and Objects

---
+++ 0x08-python-more_classes/tests/1-main.py
@@ -0,0 +1,11 @@
+s (7 sloc)  206 Bytes
+
+#!/usr/bin/python3
+Rectangle = __import__('1-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
